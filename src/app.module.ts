c3ccb5d455cded99ba22a462247a49e2f1873246
Ruben Claes <rubes.claes@gmail.com>
2020-04-30 14:08:36 +0200

    added mongo

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,25 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 import { SupermarketsModule } from './supermarkets/supermarkets.module';
 
 @Module({
-  imports: [SupermarketsModule],
+  imports: [
+    ConfigModule.forRoot({ expandVariables: true }),
+    MongooseModule.forRoot(
+      `mongodb://${process.env.MONGO_USERNAME}:${process.env.MONGO_PASSWORD}@${process.env.MONGO_URI}/${process.env.MONGO_USERNAME}`,
+      {
+        useCreateIndex: true,
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+      },
+    ),
+    SupermarketsModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
